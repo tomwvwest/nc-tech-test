4532ea8f0df025daa36c9311e5de158cb9387bbb
tomwvwest <tomwvwest@gmail.com>
2024-02-07 11:11:56 +0000

    setup environment using javascript instead of typescript

---
+++ src/__tests__/server.test.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+const { app } = require("../server");
+
+test("returns matching card title", async () => {
+  const response = await request(app).get("/cards/card001");
+
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual(
+    expect.objectContaining({
+      title: "card 1 title",
+    })
+  );
+});
